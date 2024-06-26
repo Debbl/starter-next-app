ba6fca9e8196cb1d02522cbfbe7ad76967f970dc
Brendan Dash <me@aiwan.run>
2024-06-26 20:29:33 +0800

    feat: add next-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 import { useState } from "react";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
 
   return (
     <main className="flex h-screen items-center justify-center">
-      <div
-        className="cursor-pointer select-none font-bold text-green-300"
-        onClick={() => setCount(count + 1)}
-      >
-        Hi {count}
-      </div>
+      <Button onClick={() => setCount(count + 1)}>Hi {count}</Button>
     </main>
   );
 }
