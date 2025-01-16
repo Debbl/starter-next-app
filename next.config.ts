1b8f1a8abe326699c2e3714aefd540b80c4385ad
Brendan Dash <me@aiwan.run>
2025-01-16 10:29:28 +0800

    chore: next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import bundleAnalyzer from "@next/bundle-analyzer";
 import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = bundleAnalyzer({
+  // eslint-disable-next-line n/prefer-global/process
+  enabled: process.env.ANALYZE === "true",
+});
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 const nextConfig: NextConfig = {
   output: "export",
   experimental: {
@@ -27,18 +37,7 @@ const nextConfig: NextConfig = {
   },
 };
 
-const withBundleAnalyzer = bundleAnalyzer({
-  // eslint-disable-next-line n/prefer-global/process
-  enabled: process.env.ANALYZE === "true",
-});
-
-const withSerwist = withSerwistInit({
-  swSrc: "src/app/sw.ts",
-  swDest: "public/sw.js",
-});
-
-function pipeline(fns: ((config: NextConfig) => NextConfig)[]) {
-  return fns.reduce((acc, fn) => fn(acc), nextConfig);
-}
-
-export default pipeline([withBundleAnalyzer, withSerwist]);
+export default [withBundleAnalyzer, withSerwist].reduce(
+  (config, withFn) => withFn(config),
+  nextConfig,
+);
