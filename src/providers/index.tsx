04661ced9440c347f19e5a86592bc36a91b4e3ff
Brendan Dash <me@aiwan.run>
2026-05-09 14:56:06 +0800

    chore(provider): disable serwist in development

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,13 +2,16 @@ import { domMax, LazyMotion } from 'motion/react'
 import { ThemeProvider } from 'next-themes'
 import { SerwistProvider } from './serwist-provider'
 
+// eslint-disable-next-line n/prefer-global/process
+const isDev = process.env.NODE_ENV === 'development'
+
 export interface ProvidersProps {
   children: React.ReactNode
 }
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <SerwistProvider swUrl='/sw.js'>
+    <SerwistProvider swUrl='/sw.js' disable={isDev}>
       <ThemeProvider
         attribute='class'
         defaultTheme='system'
